0f0691d89f4d78634228e2cc7d689dd2ab9c9f0e
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 14:47:20 +0530

    Add files via upload

---
+++ Problem 31.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a = 8;
+   int b = 3;
+   printf("%d\n", a*b);
+   
+   int c = 10;
+   int d = 5;
+   printf("%d\n", c*d);
+   
+   int e = 15;
+   int f = 10;
+   printf("%d\n", e*f);
+   return 0;
+}
