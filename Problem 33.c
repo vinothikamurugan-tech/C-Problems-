e8be5319cbf172a74a4938bd264b72ba04ddf1fe
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 15:14:05 +0530

    Add files via upload

---
+++ Problem 33.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int a, b, c;
+   a = 2, b = 3, c = 5;
+   printf("%d\n", a+b+c);
+   
+   int d , e , f;
+   d = 17, e = 20, f = 33;
+   printf("%d\n", d+e+f);
+   
+   int g , h , i;
+   g = 40, h = 55 , i = 60;
+   printf("%d\n", g+h+i);
+   
+   return 0;
+}
