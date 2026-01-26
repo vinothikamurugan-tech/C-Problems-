df6b8509c1df6850cf97e7e11f3bc929f0e659e5
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 13:37:33 +0530

    Add files via upload

---
+++ Problem 10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    printf("%d %d\n", a , b);
+    int c = 100;
+    int d = 120;
+    printf("%d %d\n", c , d);
+    return 0;
+}
