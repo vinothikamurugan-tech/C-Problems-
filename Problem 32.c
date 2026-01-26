c57f78050ac8b69f6381acac6fe5fb18e8c627b4
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 14:51:45 +0530

    Add files via upload

---
+++ Problem 32.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+   int a = 9;
+   int b = 2;
+   printf("Quotient = %d\n", a/b);
+   printf("Remainder = %d\n", a%b);
+   
+   int c = 10;
+   int d = 3;
+   printf("Quotient = %d\n", c/d);
+   printf("Remainder = %d\n", c%d);
+   
+   int e = 20;
+   int f = 4;
+   printf("Quotient = %d\n", e/f);
+   printf("Remainder = %d\n", e%f);
+   
+   
+   return 0;
+}
