d9b81634124586d14060971a31dd26a8b7547326
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 15:35:10 +0530

    Add files via upload

---
+++ Problem 40.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a = 10;
+   int b = 20;
+   printf("%d\n", a<b);
+   
+   int c = 10;
+   int d = 5;
+   printf("%d\n", c<d);
+   
+   int e = 6;
+   int f = 6;
+   printf("%d\n", e<f);
+   return 0;
+}
