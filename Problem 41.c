11c62b3afb43ae5e778701c720c704bb1debfb55
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-26 16:08:31 +0530

    Add files via upload

---
+++ Problem 41.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a = 20;
+   int b = 20;
+   printf("%d\n", a==b);
+   
+   int c = 10;
+   int d = 5;
+   printf("%d\n", c==d);
+   
+   int e = 0;
+   int f = 0;
+   printf("%d\n", e==f);
+   return 0;
+}
