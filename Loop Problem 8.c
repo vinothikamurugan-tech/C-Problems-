1a9b270be9daf02d1e630838af87cf05abd96c4a
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-27 11:55:03 +0530

    Add files via upload

---
+++ Loop Problem 8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, space;
+    scanf("%d", &n);
+    for(int i = n; i>=1 ; i--){
+        for(space = 1; space<=n-i; space++){
+            printf(" ");
+        }
+        for(int j = 1; j<=2*i-1 ; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
