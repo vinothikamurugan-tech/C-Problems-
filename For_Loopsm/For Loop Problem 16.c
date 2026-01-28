7166a3a582cf11e9dcbca4d21d57fb154a509ff3
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-28 15:13:45 +0530

    Add files via upload

---
+++ For_Loopsm/For Loop Problem 16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+       int n;
+       scanf("%d", &n);
+       int m = n;
+       int sum = 0;
+       for(int i = 1; i<n ; i++){
+           if(n%i==0){
+               sum = sum+i;
+           }
+       }
+       if(sum==m){
+           printf("Yes");
+       }else{
+           printf("No");
+       }
+       return 0;
+}
