bbf87f946aff198604a992acdbe0bbb0b34df74d
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-28 22:56:52 +0530

    Add files via upload

---
+++ Switch Problem 25.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int type;
+    scanf("%d", &type);
+    switch(type){
+        case(1):
+            printf("₹1200");
+            break;
+        case(2):
+             printf("₹800");
+             break;
+        case(3):
+             printf("₹500");
+             break;
+              
+    }
+    return 0;
+}
