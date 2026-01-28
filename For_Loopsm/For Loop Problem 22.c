c71a245d0024519a2c1253d4ca36d092ddd11eec
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-01-28 16:13:46 +0530

    Add files via upload

---
+++ For_Loopsm/For Loop Problem 22.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+       int n ;
+       scanf("%d", &n);
+       int count = 0;
+       for(int i = 2; i<=n ; i++){
+           if(i==2 || i==3 || i==5){
+               count = count+1;
+           }else{
+               if(i%i==0 && i%1==0 && i%2!=0 && i%3!=0 && i%5!=0){
+                   count = count+1;
+               }
+           }
+       }
+       printf("%d", count);
+       return 0;
+}
