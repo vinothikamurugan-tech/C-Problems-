5d1efa040edfc161d57f4fafe1bd87141e1c595e
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-02-02 14:22:34 +0530

    Add files via upload

---
+++ 1D_Arrays/1D Array Problem 6.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n ;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i<n ; i++){
+        scanf("%d", &a[i]);
+    }
+    int count = 0;
+    for(int i = 0; i<n ; i++){
+        int dup = 0;
+        for(int j=0 ; j<count ; j++){
+            if(a[i]==a[j]){
+                dup = 1;
+                break;
+            }
+        }
+        if(!dup){
+            printf("%d ",a[i]);
+            count++;
+        }
+    }
+    return 0;
+}
