8f6c7529c7a3bba0d959af2b143b20df0b298c3d
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-02-06 12:26:17 +0530

    Add files via upload

---
+++ 2D Array Problems/12.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    int r , c;
+    scanf("%d", &r);
+    scanf("%d", &c);
+    int a[r][c];
+    for(int i = 0; i<r ; i++){
+        for(int j = 0; j<c ; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int mincount = 1;
+    int value;
+    for(int i = 0; i<r ; i++){
+        for(int j = 0; j<c ; j++){
+            int count = 0;
+            int now = a[i][j];
+            for(int k = 0; k<r ; k++){
+                for(int m = 0 ; m<c ; m++){
+                    if(now == a[k][m]){
+                        count = count +1;
+                    }
+                }
+            }
+            if(count > mincount){
+                value = now;
+                mincount = count ;
+            }
+
+        }
+    }
+    printf("Maximum Frequency Value : %d", value);
+    return 0;
+}
