88ea70580b69bc5191ba138c452ed837d58019eb
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-02-12 09:40:54 +0530

    Add files via upload

---
+++ Function_Problems/10.c
@@ -0,0 +1,18 @@
+// Sum of Natural Numbers using Function
+#include<stdio.h>
+
+int sum(int n){
+    int sum1 = 0;
+    for(int i = n ; i>=1 ; i--){
+        sum1 = sum1 + i;
+    }
+    return sum1;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a = sum(n);
+    printf("Sum of the Natural Number is %d", a);
+    return 0;
+}
